cc68048bea3586a52592db186d8f6b11c9e32ba6
Vincenzo Ferrari <wilk3ert@gmail.com>
2018-11-12 14:18:24 +0100

    export clearCache method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export default from './components/CacheRoute'
 export CacheRoute from './components/CacheRoute'
 export CacheSwitch from './components/CacheSwitch'
-export { dropByCacheKey, getCachingKeys } from './core/manager'
+export { dropByCacheKey, getCachingKeys, clearCache } from './core/manager'
