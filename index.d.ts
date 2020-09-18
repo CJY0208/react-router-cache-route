5c1bc45b03349faf9c47318a8e2fc2146ffcd7ba
xwinstone <eejit@qq.com>
2020-09-18 10:55:54 +0800

    feat: add refreshByCacheKey method to support refreshing a route by key

--- index.d.ts
+++ index.d.ts
@@ -23,6 +23,7 @@ export interface CacheSwitchProps extends SwitchProps {
 export declare class CacheSwitch extends React.Component<CacheSwitchProps> {}
 
 export function dropByCacheKey(cacheKey: string): void
+export function refreshByCacheKey(cacheKey: string): void
 export function getCachingKeys(): Array<string>
 export function clearCache(): void
 
