9b267455e8d68e1533d1b42c9136464b92f661c2
Noi Bar <noibarr@gmail.com>
2021-06-08 13:14:51 +0300

    Revert "export get in order to enable easily controlling cached componenets"
    This reverts commit 9eb3a477cabeb1578d39a8f8649c06a36d592089.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 export default from './components/CacheRoute'
 export CacheRoute from './components/CacheRoute'
 export CacheSwitch from './components/CacheSwitch'
-export { get } from './helpers'
 export {
   dropByCacheKey,
   refreshByCacheKey,
