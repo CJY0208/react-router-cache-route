9eb3a477cabeb1578d39a8f8649c06a36d592089
Noi Bar <noibarr@gmail.com>
2021-06-08 12:58:51 +0300

    export get in order to enable easily controlling cached componenets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export default from './components/CacheRoute'
 export CacheRoute from './components/CacheRoute'
 export CacheSwitch from './components/CacheSwitch'
+export { get } from './helpers'
 export {
   dropByCacheKey,
   refreshByCacheKey,
