e5520b60d249c68b65d893241c761d3f712c99a5
Ron Hagafny <ron@mixtiles.com>
2021-08-15 08:53:30 +0300

    exporting cached navigation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export default from './components/CacheRoute'
-export CacheRoute from './components/CacheRoute'
+export CacheRoute, { cachedNavigation } from './components/CacheRoute'
 export CacheSwitch from './components/CacheSwitch'
+
 
 export {
   dropByCacheKey,
