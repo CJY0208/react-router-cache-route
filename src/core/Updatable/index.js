941cbda55af4e93423b727224b4eaf4614614f21
CJY <fanluan058@gmail.com>
2022-02-09 11:37:59 +0800

    Release 1.12.2 - fix: 延迟 freeze 时机以保证各项更新操作合理执行

---
+++ src/core/Updatable/index.js
@@ -0,0 +1,43 @@
+import React, { Component, Suspense } from 'react'
+import { Freeze as ReactFreeze } from './Freeze'
+import PropTypes from 'prop-types'
+
+import { run, get } from '../../helpers'
+
+const isSusSupported = !!Suspense
+const Freeze = isSusSupported ? ReactFreeze : ({ children }) => children
+
+export default class Updatable extends Component {
+  static propsTypes = {
+    when: PropTypes.bool.isRequired
+  }
+  state = {
+    freeze: false
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      freeze: !props.when
+    }
+  }
+
+  freezeTimeout = null
+  shouldComponentUpdate = ({ when }) => {
+    const currentWhen = this.props.when
+
+    if (when !== currentWhen) {
+      clearTimeout(this.freezeTimeout)
+      this.freezeTimeout = setTimeout(() => {
+        this.setState({
+          freeze: !when
+        })
+      }, 1000)
+    }
+    return when
+  }
+  render = () => (
+    <Freeze freeze={this.state.freeze}>
+      {run(this.props, 'children')}
+    </Freeze>
+  )
+}
